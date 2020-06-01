0d6a4df2f6b4fb023a71e4443a30ba6f201f6a64
Bamtoll09 <ryan345@nate.com>
2020-06-02 02:01:20 +0900

    v2.4a: Made Post Writing Function

--- app/src/main/java/me/bamtoll/lee/loginserver/retrofit/PostService.java
+++ app/src/main/java/me/bamtoll/lee/loginserver/retrofit/PostService.java
@@ -3,9 +3,16 @@ package me.bamtoll.lee.loginserver.retrofit;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface PostService {
     @GET("/posts/all")
     Call<List<Post>> getAll();
+
+    @POST("/posts/write")
+    @FormUrlEncoded
+    Call<Void> write(@Field("title") String title, @Field("contents") String contents);
 }
--- app/src/main/java/me/bamtoll/lee/loginserver/ui/home/WriteFragment.java
+++ app/src/main/java/me/bamtoll/lee/loginserver/ui/home/WriteFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,9 @@ import androidx.fragment.app.Fragment;
 
 import me.bamtoll.lee.loginserver.Main3Activity;
 import me.bamtoll.lee.loginserver.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WriteFragment extends Fragment {
 
@@ -21,6 +27,16 @@ public class WriteFragment extends Fragment {
         ((Main3Activity) getContext()).displayFab(false);
 
         View root = inflater.inflate(R.layout.fragment_write, container, false);
+        Button writeBtn = root.findViewById(R.id.btn_write);
+        writeBtn.setOnClickListener(v -> {
+            EditText titleEdit = root.findViewById(R.id.edit_write_title);
+            EditText contentsEdit = root.findViewById(R.id.edit_write_contents);
+
+            String title = titleEdit.getText().toString();
+            String contents = contentsEdit.getText().toString();
+
+            write(title, contents);
+        });
 
         return root;
     }
@@ -29,7 +45,22 @@ public class WriteFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
 
-
         ((Main3Activity) getContext()).displayFab(true);
+    }
+
+    public void write(String tit, String con) {
+        ((Main3Activity) getContext()).service.write(tit, con).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(getContext().getApplicationContext(), "WRITE SUCCESS!", Toast.LENGTH_SHORT).show();
+                ((Main3Activity) getContext()).onBackPressed();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getContext().getApplicationContext(), "WRITE FAIL!", Toast.LENGTH_SHORT).show();
+                ((Main3Activity) getContext()).onBackPressed();
+            }
+        });
     }
 }
